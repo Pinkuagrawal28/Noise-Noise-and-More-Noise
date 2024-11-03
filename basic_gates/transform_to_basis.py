60c3cd73a8eac57a5466f605bdb41f97ea3a9010
Pinku Agrawal <92142824+Pinkuagrawal28@users.noreply.github.com>
2024-11-03 17:57:42 +0530

    Update transform_to_basis.py
    fixed changes for basis transform

--- basic_gates/transform_to_basis.py
+++ basic_gates/transform_to_basis.py
@@ -9,18 +9,21 @@ def transform_to_basis(qc: QuantumCircuit) -> QuantumCircuit:
     """
     transformed_qc = QuantumCircuit(*qc.qregs)
     
-    for inst, qargs, _ in qc.data:
-        if inst.name == 'h':
+    for instr in qc.data:
+        operation = instr.operation  # Get the operation
+        qargs = instr.qubits          # Get the qubits
+        
+        if operation.name == 'h':
             # Replace Hadamard with RZ and SX (approximate decomposition)
             transformed_qc.rz(np.pi / 2, qargs[0])
             transformed_qc.sx(qargs[0])
             transformed_qc.rz(np.pi / 2, qargs[0])
-        elif inst.name == 't':
+        elif operation.name == 't':
             # Replace T with an RZ gate
             transformed_qc.rz(np.pi / 4, qargs[0])
-        elif inst.name == 'cx':
+        elif operation.name == 'cx':
             transformed_qc.cx(qargs[0], qargs[1])
         else:
-            transformed_qc.append(inst, qargs)
+            transformed_qc.append(operation, qargs)
     
     return transformed_qc
